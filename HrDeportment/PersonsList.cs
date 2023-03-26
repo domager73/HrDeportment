55aef3a65caba7e6914eeb9540cdc161acd5a57b
domager73 <112970685+domager73@users.noreply.github.com>
2023-03-26 12:53:41 +0300

    Add persons list

---
+++ HrDeportment/PersonsList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrDeportment
+{
+    public class PersonsList
+    {
+        private List<Person> _persons;
+
+        public PersonsList() 
+        {
+            _persons = new List<Person>() { 
+                new Person(1, "Dima", 2, 1000, PostStorage.Posts[1]),
+                new Person(2, "Diwa", 2, 2000, PostStorage.Posts[3]),
+                };
+        }
+
+        public void AddNew(Person person) 
+        {
+            _persons.Add(person);
+        }
+    }
+}
---
+++ HrDeportment/PostStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrDeportment
+{
+    public class PostStorage
+    {
+        private List<string> _posts;
+
+        public PostStorage() 
+        {
+            _posts = new List<string>() { "Деректор", "Уборщица", "Сеньер", "Мидле", "Джун"};
+        }
+
+        List<string> GetPosts() => _posts;
+    }
+}
